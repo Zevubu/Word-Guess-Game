05140b90adad614e5506af9d58871c51ded188f4
Zev Hoffman <zevubu@gmail.com>
2019-07-06 21:38:39 -0700

    added index.html assests folder with css w/style.css&resets.css and javascript w/game.js sub-folders. also started working of js.

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+let devine = {
+    title: "Devine",
+    dateOrAge: "older then you'll ever be!",
+    about: "The Filthiest person who ever lived!",
+    Quotes: [
+        "filler", "filler", "full",
+    ]
+};
+
+alert(devine.title)
