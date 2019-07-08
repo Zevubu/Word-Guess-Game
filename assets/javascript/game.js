2d3f663950106a93acc245d5fb2b93dec61ff820
Zev Hoffman <zevubu@gmail.com>
2019-07-08 13:59:00 -0700

    added atributes and ids to index.html. added more objects to js, and a list of the objects attempted to creat random word sectors from list.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,47 @@
+let wins = 0;
+let numOfGeuss = 12; 
 
 let devine = {
     title: "Devine",
-    dateOrAge: "older then you'll ever be!",
-    about: "The Filthiest person who ever lived!",
-    Quotes: [
+    dateOrAge: "Born: October 19, 1945. Died: March 7, 1988",
+    hint: "The Filthiest person who ever lived!",
+    quotes: [
         "filler", "filler", "full",
-    ]
+    ],
+    objectImage: "filler",
+    sound: "filler",
 };
 
-alert(devine.title)
+let femaleTrouble = {
+    title: "Female Trouble"
+    dateOrAge: "October 4, 1974"
+}
+
+let edith = {
+    title:"Edith Massey"
+    dateOrAge:"Born: May 28, 1918. Died: October 24, 1984"
+}
+// alert(devine.title)
+
+let currentWord = [
+    devine.title,
+    femaleTrouble.title,
+    edith.title,
+];
+
+chosenWord = currentWord [Math.floor(Math.random() * currentWord. length)];
+word = chosenWord[Math.floor(math.random() * chosenWord.length)];
+word = word.replace("-");
+
+currentWordIndex = 0;
+
+// if 
+
+const currentWordlist = () =>{
+    if(currentWordIndex <= currentWord.length -1){
+        document.querySelector("#currentWord"). innerHTML = word[currentWordIndex];
+    }
+}
+
+// console.log(currentWordList);
+
